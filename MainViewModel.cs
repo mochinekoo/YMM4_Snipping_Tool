aa13bf246102e036648d67d8f760f6db1a13002c
mochinekoo <mochinekoo0113@gmail.com>
2026-05-17 12:50:50 +0900

    [add] プラグインが認識されるようになりました

---
+++ MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YMM4_Snipping_Tool {
+    internal class MainViewModel {
+    }
+}
---
+++ PluginMain.cs
@@ -0,0 +1,9 @@
+﻿using YukkuriMovieMaker.Plugin;
+
+namespace YMM4_Snipping_Tool {
+    public class PluginMain : IToolPlugin {
+        public Type ViewModelType => typeof(MainViewModel);
+        public Type ViewType => typeof(MainWindow);
+        public string Name => "スクショプラグイン";
+    }
+}
